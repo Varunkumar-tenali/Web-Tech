3c97bdccf46f04f30fb2617f13a0b622964e1341
Varun Kumar <varunkumartenali@gmail.com>
2026-04-02 11:15:21 +0530

    Delete Lab9 directory

--- Lab9/my-form-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import UserForm from "./UserForm.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <UserForm />
-  </StrictMode>
-);
